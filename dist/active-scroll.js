2b44fe85688d68fbe569c44d99d232b5db385465
Gareth Cozens <gareth@cuzzy.co.za>
2016-04-08 09:57:31 +0200

    Add Travis CI and compression

--- dist/active-scroll.js
+++ dist/active-scroll.js
@@ -5,98 +5,116 @@
  * Author: Gareth Cozens (Cuzzy)
  */
 
-(function (factory) {
+(function( factory )
+{
     'use strict';
 
-    if (typeof define === 'function' && define.amd) {
-        define(['jquery'], factory);
-    } else if (typeof exports === 'object') {
-        module.exports = factory(require('jquery'));
-    } else {
-        factory(jQuery);
+    if ( typeof define === 'function' && define.amd )
+    {
+        define( [ 'jquery' ], factory );
+    } else if ( typeof exports === 'object' )
+    {
+        module.exports = factory( require( 'jquery' ) );
+    } else
+    {
+        factory( jQuery );
     }
 
-}(function ($) {
+}( function( $ )
+{
     'use strict';
 
     var settings = {
-        data : 'scroll',
-        active : 'active',
-        offset : 20,
+        data    : 'scroll',
+        active  : 'active',
+        offset  : 20,
         animate : 1000
     };
 
-    var items,
+    var element,
+        items,
         sections = [],
         firstKey = '';
 
     var ActiveScroll =
-    {
-        closest : function( pos )
         {
-            var current = sections[firstKey],
-                diff = Math.abs ( pos - current );
+            closest : function( pos )
+            {
+                var current = sections[ firstKey ],
+                    diff    = Math.abs( pos - current );
 
-            for (var key in sections)
-            {
-                var result = Math.abs ( pos - sections[ key ] );
-                if ( result < diff )
-                {
-                    diff = result;
-                    current = sections[ key ];
-                }
-            }
-            return current;
-        },
-        attach : function()
-        {
-            //Active Highlighting
-            $( window ).scroll( function()
-            {
-                var pos = $( window ).scrollTop();
-                var active = ActiveScroll.closest( pos );
-                items.removeClass( settings.active );
-                var highlight = settings.firstKey;
                 for ( var key in sections )
                 {
-                    if ( sections[key] === active )
+                    var result = Math.abs( pos - sections[ key ] );
+                    if ( result < diff )
                     {
-                        highlight = key;
-                        break;
+                        diff    = result;
+                        current = sections[ key ];
                     }
                 }
-                $('[data-' + settings.data + '="' + highlight + '"' ).addClass( settings.active);
-            });
-
-            //Scroll To Click Event
-            items.click( function(event)
+                return current;
+            },
+            attach  : function()
             {
-                $( "html, body" ).animate({
-                    scrollTop: $("#" + $( this ).data( settings.data )).offset().top - settings.offset
-                }, settings.animate);
-                event.stopImmediatePropagation();
-            });
-        },
-        init : function( el, options )
-        {
-            items = $( el ).find( "[data-" + settings.data + "]" );
-            settings = $.extend(settings, options);
-            //find all items and get their element's position relative to top of page
-            var count = 0;
-            items.each( function()
-            {
-                var item = $( this ).data( settings.data );
-                sections[item] = $( "#" + item ).offset().top;
-                firstKey = (count === 0) ? item : firstKey;
-                count++;
-            });
+                //Active Highlighting
+                $( window ).scroll( function()
+                {
+                    var pos    = $( window ).scrollTop();
+                    var active = ActiveScroll.closest( pos );
+                    items.removeClass( settings.active );
+                    var highlight = settings.firstKey;
+                    for ( var key in sections )
+                    {
+                        if ( sections[ key ] === active )
+                        {
+                            highlight = key;
+                            break;
+                        }
+                    }
+                    $( '[data-' + settings.data + '="' + highlight + '"]' ).addClass( settings.active );
+                } );
 
-            ActiveScroll.attach();
-        }
-    };
+                //Scroll To Click Event
+                $( "[data-" + settings.data + "]" ).click( function( event )
+                {
+                    var id = $( this ).data( settings.data );
+                    $( "html, body" ).animate( {
+                        scrollTop : $( "#" + id ).offset().top - settings.offset
+                    }, settings.animate, function()
+                    {
+                        //Highlight if page can't scroll to element
+                        var current = $( window ).scrollTop() + settings.offset;
+                        console.log( current, sections[ id ] );
+                        if ( current !== sections[ id ] )
+                        {
+                            items.removeClass( settings.active );
+                            $( element ).find( '[data-' + settings.data + '="' + id + '"]' ).addClass( settings.active );
+                        }
+                    } );
+                    event.stopImmediatePropagation();
+                } );
+            },
+            init    : function( el, options )
+            {
+                element   = el;
+                items     = $( element ).find( "[data-" + settings.data + "]" );
+                settings  = $.extend( settings, options );
+                //Find all items and get their element's position relative to top of page
+                var count = 0;
+                items.each( function()
+                {
+                    var item         = $( this ).data( settings.data );
+                    sections[ item ] = $( "#" + item ).offset().top;
+                    firstKey         = (count === 0) ? item : firstKey;
+                    count++;
+                } );
+
+                ActiveScroll.attach();
+            }
+        };
 
     $.fn.activescroll = function( options )
     {
         return ActiveScroll.init( this, options );
     };
-}));
+} ));
---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+
+var original = 'dist/active-scroll.js',
+    output = 'dist';
+
+gulp.task('compress', function() {
+    return gulp.src(original)
+        .pipe(uglify())
+        .pipe(rename('active-scroll.min.js'))
+        .pipe(gulp.dest( output ));
+});
